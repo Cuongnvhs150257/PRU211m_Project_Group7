95cdab2c5af600ee0fec020368a9b6df28daea80
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-03-11 17:08:30 +0700

    Generate Enemy Basic

---
+++ Github/Assets/Scripts/ScriptEnemy/BulletEnemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEnemy : MonoBehaviour
+{
+    GameObject target;
+    public float speed;
+    Rigidbody2D bulletEB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletEB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector2 moveDir = (target.transform.position - transform.position).normalized*speed;
+        bulletEB.velocity = new Vector2(moveDir.x, moveDir.y);
+        Destroy(this.gameObject, 2);
+    }
+
+
+}
