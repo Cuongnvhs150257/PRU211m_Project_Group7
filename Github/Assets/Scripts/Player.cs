46358e0383bba25dda8aec9360764f50bf026854
Cuongnvhs150257 <97275308+Cuongnvhs150257@users.noreply.github.com>
2023-03-11 16:53:27 +0700

    up base project again

---
+++ Github/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    public Vector3 moveInput;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        if(moveInput.x != 0)
+        {
+            if(moveInput.x > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 0);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 0);
+            }
+        }
+    }
+}
