b18ef888d7cf557bb636606014eca3637bd5b262
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-03-14 23:48:57 +0700

    Upgrade Creep3

---
+++ Github/Assets/Scripts/ScriptEnemy/Creep3Enemy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creep3Enemy : MonoBehaviour
+{
+    const int SpawnBorderSize = 100;
+    int minSpawnX;
+    int minSpawnY;
+    int maxSpawnX;
+    int maxSpawnY;
+    Timer spawnTimer;
+    [SerializeField]
+    GameObject enemy;
+    void Start()
+    {
+        minSpawnX = SpawnBorderSize;
+        maxSpawnX = Screen.width - SpawnBorderSize;
+        minSpawnY = SpawnBorderSize;
+        maxSpawnY = Screen.height - SpawnBorderSize;
+
+        //create and start timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = 2;
+        spawnTimer.Run();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (spawnTimer.Finished)
+        {
+            SpawnObject();
+            spawnTimer.Duration = 4;
+            spawnTimer.Run();
+        }
+    }
+
+    void SpawnObject()
+    {
+        Vector3 location = new Vector3(Random.Range(minSpawnX, maxSpawnX), Random.Range(minSpawnY, maxSpawnY),
+               -Camera.main.transform.position.z);//z should be zero
+        Vector3 worldLocation = Camera.main.ScreenToWorldPoint(location);
+        GameObject objectRender = Instantiate<GameObject>(enemy, worldLocation, Quaternion.identity); ;
+    }
+}
