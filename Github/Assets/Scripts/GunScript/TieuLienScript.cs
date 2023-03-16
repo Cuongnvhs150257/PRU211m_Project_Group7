dfc56b1cebadbbd1b99f33fd215b00847584c20b
AsheS <campus265@gmail.com>
2023-03-16 15:59:27 +0700

    update format gun direct to enemies

--- Github/Assets/Scripts/GunScript/TieuLienScript.cs
+++ Github/Assets/Scripts/GunScript/TieuLienScript.cs
@@ -8,11 +8,12 @@ public class TieuLienScript : MonoBehaviour
     public GameObject bullet;
     public Transform bulletTransform;
     public Transform nongsung;
-    public bool canFire;
     private float timer;
-    public static float timeBetweenFire = 1f;
+    public float timeBetweenFire;
     private Rigidbody2D rb;
     Vector2 spawnLocation = Vector2.zero;
+    private float yPositionForRotate = 0.1938882f;
+    private float xPosiotionForRotate = 0.2431382f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +24,7 @@ public class TieuLienScript : MonoBehaviour
     void Update()
     {
         GameObject enemy = getNearestEnemy();
-        if(enemy != null)
+        if (enemy != null)
         {
             Vector2 direction = enemy.transform.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
@@ -32,20 +33,32 @@ public class TieuLienScript : MonoBehaviour
             // Set the rotation of this object to the opposite angle
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, oppositeAngle));
 
-            timer += Time.deltaTime;
-            if (timer >= 0.5)
-            {
-                GameObject shot = Instantiate<GameObject>(bullet, transform.position, bulletTransform.localRotation);
-                shot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, oppositeAngle));
-                //Debug.Log("addforce ne: " + (targetObject.transform.position - transform.position));
-                shot.GetComponent<Rigidbody2D>().AddForce((nongsung.transform.position - transform.position) * 10, ForceMode2D.Impulse);
-                timer = 0;
-            }
+            if (transform.eulerAngles.z > 90 && transform.eulerAngles.z < 270)
+        {
+            Vector3 vtr = new Vector3(transform.localScale.x, -transform.localScale.y, 0);
+            transform.localScale = new Vector3(xPosiotionForRotate, -yPositionForRotate, 0);
+            Debug.Log("Ket qua: " + transform.localScale);
         }
-        
-    }
+        else
+        {
+            Vector3 vtr = new Vector3(transform.localScale.x, transform.localScale.y, 0);
+            transform.localScale = new Vector3(xPosiotionForRotate, yPositionForRotate, 0);
+        }
 
-    GameObject getNearestEnemy()
+
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenFire)
+        {
+            GameObject shot = Instantiate<GameObject>(bullet, transform.position, bulletTransform.localRotation);
+            shot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, oppositeAngle));
+            //Debug.Log("addforce ne: " + (targetObject.transform.position - transform.position));
+            shot.GetComponent<Rigidbody2D>().AddForce((nongsung.transform.position - transform.position) * 10, ForceMode2D.Impulse);
+            timer = 0;
+        }
+    }
+}
+
+    private GameObject getNearestEnemy()
     {
         float currentDistance = float.MaxValue;
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemies");
@@ -60,6 +73,7 @@ public class TieuLienScript : MonoBehaviour
                 currentDistance = objectDistance;
             }
         }
+
         return targetEnemy;
     }
 
