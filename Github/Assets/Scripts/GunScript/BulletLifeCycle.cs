621542d0e8911eb838912f17d6e7e6bd0f38b97f
AsheS <campus265@gmail.com>
2023-03-20 13:44:09 +0700

    format code bullet

--- Github/Assets/Scripts/GunScript/BulletLifeCycle.cs
+++ Github/Assets/Scripts/GunScript/BulletLifeCycle.cs
@@ -18,7 +18,7 @@ public class BulletLifeCycle : MonoBehaviour
     private float timer2;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -38,49 +38,10 @@ public class BulletLifeCycle : MonoBehaviour
         if (other.gameObject.CompareTag("enemies"))
         {
             //tru di dame cua sung
-            obj.GetComponent<EnemyProperties>().Hp-= dame;
+            obj.GetComponent<EnemyProperties>().Hp -= dame;
             if (obj.gameObject.GetComponent<EnemyProperties>().Hp <= 0)
             {
                 Destroy(obj.gameObject);
-                mainCharacter.GetComponent<ManageLevel>().Exp += obj.gameObject.GetComponent<EnemyProperties>().Exp;
-                //xu li exp
-
-                mainCharacter.GetComponent<PowerManage>().Count++;
-
-                //dkien xhien vong no
-                if (mainCharacter.GetComponent<PowerManage>().Count == 3)
-                {
-                    mainCharacter.GetComponent<PowerManage>().vongNo.SetActive(true);
-                    mainCharacter.GetComponent<PowerManage>().isPower = true;
-                }
-                Debug.Log("Count: " + mainCharacter.GetComponent<PowerManage>().Count);
-
-                if (mainCharacter.GetComponent<PowerManage>().isPower == true)
-                {
-                    timer2 += Time.deltaTime;
-                    if (timer2 >= 10)
-                    {
-                        mainCharacter.GetComponent<PowerManage>().isPower = false;
-                        timer2 = 0;
-                        mainCharacter.GetComponent<PowerManage>().Count = 0;
-                    }
-                }
-                
-
-
-                var exp = mainCharacter.GetComponent<ManageLevel>().Exp;
-                var nextLevel = mainCharacter.GetComponent<ManageLevel>().NextLv;
-                if (exp >=nextLevel)
-                {
-                    int rs = exp - nextLevel;//so du exp
-                    mainCharacter.GetComponent<ManageLevel>().Level++;
-                    mainCharacter.GetComponent<ManageLevel>().levelCount.text = mainCharacter.GetComponent<ManageLevel>().Level.ToString();
-                    Debug.Log("Level up: " + mainCharacter.GetComponent<ManageLevel>().Level);
-                    mainCharacter.GetComponent<ManageLevel>().NextLv += 5;
-                    mainCharacter.GetComponent<ManageLevel>().Exp = rs;
-                    
-                }
-                
             }
 
             //destroy bullet
