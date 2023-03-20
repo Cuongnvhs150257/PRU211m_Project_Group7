170732071b5436803b9f6d1aa9d5078d98f852ed
Cuongnvhs150257 <97275308+Cuongnvhs150257@users.noreply.github.com>
2023-03-20 15:15:13 +0700

    add audio button

--- Github/Assets/Scripts/MenuScript/MainMenu.cs
+++ Github/Assets/Scripts/MenuScript/MainMenu.cs
@@ -7,7 +7,13 @@ public class MainMenu : MonoBehaviour
 {
     public void HandlePlayButtonClickEvent()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("GamePlay");
+    }
+
+    public void HandleAudioButtonClickEvent()
+    {
+        SceneManager.LoadScene("VolumeMenu");
     }
 
     public void HandleQuitButtonClickEvent()
---
+++ Github/Assets/Scripts/MenuScript/VolumeControl.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    [SerializeField]
+    public Slider volumeSlider = null;
+
+    [SerializeField]
+    public Text volumeText = null;
+
+    void Start()
+    {
+        LoadValues();
+    }
+
+    public void VolumeSlider(float volume)
+    {
+        volumeText.text = volume.ToString("0.0");
+
+    }
+
+    public void SaveVolume()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+    public void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
+
+    public void HandleBackButtonClickEvent()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
+
+
+}
